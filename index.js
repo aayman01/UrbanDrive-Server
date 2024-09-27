c37b604deee8cd21b191a3db4c97a81b10089ffd
aayman01 <ayman.alfi098@gmail.com>
2024-09-27 10:14:49 +0600

    fixed some bugs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,7 +10,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xrbh57q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -23,68 +22,85 @@ const client = new MongoClient(uri, {
   },
 });
 
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
 
     const usersCollection = client.db("urbanDrive").collection("users");
     const carsCollection = client.db("urbanDrive").collection("cars");
-    
-     
-    app.get('/cars',async(req,res)=>{
+
+
+    app.get("/cars/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const car = await carsCollection.findOne(query);
+      res.send(car);
+    });
+
+
+    app.get("/cars", async (req, res) => {
       const page = parseInt(req.query.page) || 1; // Default to 1 if not provided
-        const limit = parseInt(req.query.limit) || 6;
-        const skip = (page-1)*limit;
+      const limit = parseInt(req.query.limit) || 6;
+      const skip = (page - 1) * limit;
 
-        const categoryName = req.query.category || '';
-        const minPrice = parseFloat(req.query.minPrice) || 0;
-        const maxPrice = parseFloat(req.query.maxPrice) || Number.MAX_SAFE_INTEGER;
-        const sortOption = req.query.sort || ''; 
-        const seatCount = parseInt(req.query.seatCount) || null;
+      const categoryName = req.query.category || "";
+      const minPrice = parseFloat(req.query.minPrice) || 0;
+      const maxPrice =
+        parseFloat(req.query.maxPrice) || Number.MAX_SAFE_INTEGER;
+      const sortOption = req.query.sort || "";
+      const seatCount = parseInt(req.query.seatCount) || null;
 
-        try {
+      try {
         const query = {
-                
-                ...(categoryName && { category: { $regex: categoryName, $options: 'i' } }),
-                // ...(categoryName && { category: { $regex: categoryName, $options: 'i' } }),
-                price: { $gte: minPrice, $lte: maxPrice },
-                ...(seatCount && { seatCount: { $gte: seatCount } })
-              };
-              let sort = {};
-              if (sortOption === 'price-asc') {
-                  sort = { price: 1 }; // Sort by price ascending
-              } else if (sortOption === 'price-desc') {
-                  sort = { price: -1 }; // Sort by price descending
-              } else if (sortOption === 'date-desc') {
-                sort = { date: -1 }; // Sort by date descending (newest first)
-            } else if (sortOption === 'date-asc') {
-                sort = { date: 1 };}
-      
-         // Fetch total cars count without pagination
-         const totalCars = await carsCollection.countDocuments();
-            console.log("totalcars:",totalCars)
+          ...(categoryName && {
+            category: { $regex: categoryName, $options: "i" },
+          }),
+          // ...(categoryName && { category: { $regex: categoryName, $options: 'i' } }),
+          price: { $gte: minPrice, $lte: maxPrice },
+          ...(seatCount && { seatCount: { $gte: seatCount } }),
+        };
+        let sort = {};
+        if (sortOption === "price-asc") {
+          sort = { price: 1 }; // Sort by price ascending
+        } else if (sortOption === "price-desc") {
+          sort = { price: -1 }; // Sort by price descending
+        } else if (sortOption === "date-desc") {
+          sort = { date: -1 }; // Sort by date descending (newest first)
+        } else if (sortOption === "date-asc") {
+          sort = { date: 1 };
+        }
+
+        // Fetch total cars count without pagination
+        const totalCars = await carsCollection.countDocuments();
+        // console.log("totalcars:", totalCars);
 
         // Fetch cars with pagination
-        
-        const Cars = await carsCollection.find(query).sort(sort).skip(skip).limit(limit).toArray();
-          // Calculate total pages
-          const totalPages = Math.ceil(totalCars / limit);
-      
-          // Send the paginated data along with totalPages and totalCars
-          console.log("Incoming query parameters:", req.query);
-       
-          res.json({ Cars,totalCars, totalPages, totalCars, currentPage: page });
-        } catch (error) {
-          res.status(500).json({ message: 'Server error', error });
-        }
-    })
+
+        const Cars = await carsCollection
+          .find(query)
+          .sort(sort)
+          .skip(skip)
+          .limit(limit)
+          .toArray();
+        // Calculate total pages
+        const totalPages = Math.ceil(totalCars / limit);
+
+        // Send the paginated data along with totalPages and totalCars
+        // console.log("Incoming query parameters:", req.query);
+
+        res.json({ Cars, totalCars, totalPages, currentPage: page });
+      } catch (error) {
+        res.status(500).json({ message: "Server error", error });
+      }
+    });
 
     // user related api
     app.post("/users", async (req, res) => {
       const user = req.body;
-      const query = { email: user.email };
+      const query = { email: user?.email };
       const existUser = await usersCollection.findOne(query);
       if (existUser) {
         return res.send({ message: "user already exists", insertedId: null });
@@ -103,7 +119,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
 
 app.get("/", (req, res) => {
   res.send("Urban drive is running...");
