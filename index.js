9db6f1282c967d16f0b5744b442646f9a67174c8
nayeem-miah <nayeem5113a@gmail.com>
2024-09-27 11:13:50 +0600

    working on payment system

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 app.use(cors());
 
