d6a010460d71425e1c5c3fadf461c5fe6e61eef4
roksana35 <roksana7235@gmail.com>
2024-10-06 21:39:27 +0600

    pull request to development

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const crypto = require('crypto');
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 app.use(cors());
 
